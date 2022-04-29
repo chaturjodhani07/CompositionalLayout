39d99a3bc9d436afeed6a60bfac4988cdba387c8
hardik23682 <hardik.rkcet@gmail.com>
2022-04-29 11:25:51 +0530

    Initial Commit

---
+++ CompositionalLayout/CompositionalLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompositionalLayoutApp.swift
+//  CompositionalLayout
+//
+//  Created by Chirag on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CompositionalLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
